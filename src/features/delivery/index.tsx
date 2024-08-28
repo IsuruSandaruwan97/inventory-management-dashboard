dac40c2111e6e79113f011fcdf1fa56b4bb5f3b3
IsuruSandaruwan97 <isuruskarunarathne@gmail.com>
2024-08-28 15:17:55 +0530

    feat: Implement store feature

--- src/features/delivery/index.tsx
+++ src/features/delivery/index.tsx
@@ -1,7 +1,83 @@
 /** @format */
 
+import Table from "@components/Table";
+import { StyleSheet } from "@configs/stylesheet";
+import { KeyValuePair } from "@configs/types";
+import { Card, Space, DatePicker, Radio, Row } from "antd";
+import { TableProps } from "antd/es/table/InternalTable";
+const { RangePicker } = DatePicker;
+
+const DateOptions: KeyValuePair[] = [
+  { label: "Today", value: "today" },
+  { label: "This Week", value: "week" },
+  { label: "This Month", value: "month" },
+];
+
+const columns: TableProps<any>["columns"] = [
+  { title: "Item Code", dataIndex: "id", key: "id", responsive: ["lg"] },
+  {
+    title: "Image",
+    dataIndex: "image",
+    key: "image",
+    render: (value) => <img src={value} height={40} width={40} />,
+  },
+  {
+    title: "Item Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Item Category",
+    dataIndex: "category",
+    key: "category",
+  },
+  {
+    title: "Quantity",
+    dataIndex: "quantity",
+    key: "quantity",
+  },
+  {
+    title: "Date",
+    dataIndex: "date",
+    key: "date",
+  },
+  {
+    title: "Delivery By",
+    dataIndex: "deliveryBy",
+    key: "deliveryBy",
+  },
+];
+
 const Delivery = () => {
-  return <>delivery</>;
+  return (
+    <Space direction="vertical" style={styles.space}>
+      <Card>
+        <Row style={styles.actionBar}>
+          <Space>
+            <Radio.Group defaultValue={DateOptions[0].value}>
+              {DateOptions?.map((item) => (
+                <Radio.Button value={item.value} key={`option_${item}`}>
+                  {item?.label}
+                </Radio.Button>
+              ))}
+            </Radio.Group>
+            <RangePicker />
+          </Space>
+        </Row>
+      </Card>
+      <Table columns={columns} dataSource={[]} />
+    </Space>
+  );
 };
 
 export default Delivery;
+
+const styles = StyleSheet.create({
+  space: {
+    width: "100%",
+  },
+
+  actionBar: {
+    justifyContent: "flex-end",
+  },
+});
--- src/features/store/index.tsx
+++ src/features/store/index.tsx
@@ -1,24 +1,49 @@
 /** @format */
 
-import { Card } from "antd";
-import ItemTable from "./components/ItemTable";
+import { Button, Card, Row, Segmented, Space } from "antd";
+import ItemTable from "@features/store/components/ItemTable";
 import { StyleSheet } from "@configs/stylesheet";
-
+import { useState } from "react";
+import ReturnItemsFormModal from "@features/store/components/forms/ReturnItemsForm";
+const options = ["Store", "Delivery"];
 const Store = () => {
+  const [option, setOption] = useState<string>(options[0]);
+  const [showReturnModal, setShowReturnModal] = useState<boolean>(false);
   return (
     <>
-      <Card>A</Card>
-      <Card style={style.itemTableCard}>
+      <Card>
+        <Row style={styles.actionBar}>
+          <Space>
+            <Button>Add for Delivery</Button>
+            <Button onClick={() => setShowReturnModal(true)}>
+              Return Items
+            </Button>
+          </Space>
+          <Segmented
+            options={options}
+            value={option}
+            onChange={(value) => setOption(value)}
+          />
+        </Row>
+      </Card>
+      <Card style={styles.itemTableCard}>
         <ItemTable />
       </Card>
+      {showReturnModal && (
+        <ReturnItemsFormModal
+          open={showReturnModal}
+          onCancel={() => setShowReturnModal(false)}
+        />
+      )}
     </>
   );
 };
 
 export default Store;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   itemTableCard: {
     marginTop: 8,
   },
+  actionBar: { justifyContent: "space-between" },
 });
